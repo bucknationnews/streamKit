89e07fe596686e4adabfd97ee573893348b32a4b
Hampton <k4kfh.radio@gmail.com>
2016-04-08 09:01:10 -0500

    Adding new auto-installer scripts
    The new bat scripts don't have to be moved, they detect whether or not
    you have VLC and automatically make a shortcut accordingly.

--- batch/admins/Buck Nation News.bat
+++ batch/admins/Buck Nation News.bat
@@ -1,1 +1,11 @@
-start /d "C:\Program Files\VideoLAN\VLC\" vlc.exe udp://@239.1.1.42:1234/
+@ECHO off
+IF EXIST "C:\Program Files\VideoLAN\VLC\" set vlc64=true & set vlc=true
+IF EXIST "C:\Program Files (x86)\VideoLAN\VLC\" set vlc32=true & set vlc=true
+
+IF DEFINED vlc64 echo start /d "C:\Program Files\VideoLAN\VLC\" vlc.exe udp://@239.1.1.44:1234/ > %UserProfile%\Desktop\Buck^ Nation^ News.bat
+IF DEFINED vlc32 echo start /d "C:\Program Files (x86)\VideoLAN\VLC\" vlc.exe udp://@239.1.1.44:1234/ > %UserProfile%\Desktop\Buck^ Nation^ News.bat
+IF DEFINED vlc32 echo We detected that you have VLC Media Player (32 bit) installed. & echo. & echo A shortcut to the BNN Live Stream has been placed on your desktop. It's named "Buck Nation News Live Stream." You can click that to launch the stream. & echo. & echo Please let Ms. Sullins or Hampton know if you have any problems!
+IF DEFINED vlc64 echo We detected that you have VLC Media Player (64 bit) installed. & echo. & echo A shortcut to the BNN Live Stream has been placed on your desktop. It's named "Buck Nation News Live Stream." You can click that to launch the stream. & echo. & echo Please let Ms. Sullins or Hampton know if you have any problems!
+IF NOT DEFINED vlc echo You don't have VLC Media Player. Please have Ms. Sullins or a broadcasting student come install VLC on your computer.
+echo.
+pause
